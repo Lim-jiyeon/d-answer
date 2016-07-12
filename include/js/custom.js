ff52caf0718ffb4afcb17171fcbeb7231b023e00
Lim Jiyeon <jyeon83@gmail.com>
2016-07-13 01:22:42 +0900

    Best Seller 화면 추가, header 드롭메뉴 겹치는 이슈 해결

--- include/js/custom.js
+++ include/js/custom.js
@@ -4,13 +4,12 @@ $(document).ready(function() {
     // --------------------------------------------------
     $(".nav > li > a").on({
         'mouseenter focusin': function() {
-            $(this).closest("li").siblings().find('.acc').stop().stop().stop().fadeTo(50, 0, function() {
+            $(this).closest("li").siblings().find('.selected').stop().stop().stop().fadeTo(50, 0, function() {
                 $(this).hide().removeClass('selected');
             });
             $(this).siblings('.nav-child').show().addClass('selected').stop().stop().fadeTo(100, 1);
         },
         'focusout mouseleave': function() {
-            $(this).siblings().hide();
             $(this).siblings('.nav-child').delay(1000).fadeTo(500, 0, function() {
                 $(this).hide().removeClass('selected');
             });
