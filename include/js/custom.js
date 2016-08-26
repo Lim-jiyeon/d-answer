26fc0ff3617afd6bbbb822518a92e3dee3e5796c
jyeon <jyeon@isecommerce.co.kr>
2016-08-26 11:01:47 +0900

    스크롤 따라다리는 영역(header, 상세 우측) position 수정

--- include/js/custom.js
+++ include/js/custom.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
 
+    var headerTop = $('.header');
+     $(window).scroll(function(){
+         headerTop.stop();
+         headerTop.animate( { "top": $(document).scrollTop() + 0 }, 0 );
+     });
+
     // Menu
     // --------------------------------------------------
     $(".nav > li > a").on({
@@ -58,60 +64,32 @@ $(document).ready(function() {
 
     // 상품상세 스크롤 따라다니는 상품 옵션
     // --------------------------------------------------
-    // $(".detail-shop-inner").sticky({
-    //     topSpacing: 150,
-    //     bottomSpacing: 500
-    // });
-    // $(".detail-shop-info").animate( { "top": $(document).scrollTop() + 88 +"px" }, 500 ); // 빼도 된다.
-
-
-    // var aaa = $(".detail-container").height();
-    // console.log(aaa);
-    // $(window).scroll(function(){
-    //     $(".detail-shop-inner").stop();
-    //     $(".detail-shop-inner").animate( { "top": $(document).scrollTop() + 88 + "px" }, 1000 );
-    //     var offset = $(".detail-shop-inner").offset().top;
-    //     console.log(offset);
-        // if(aaa <= offset) {
-        //     $(".detail-shop-info").addClass("fixed");
-        // } else {
-        //     $(".detail-shop-info").removeClass("fixed");
-        // }
-    // });
-
-// $(".detail-shop-info").stick_in_parent();
-
-$(function() {
-
-    var $sidebar   = $(".detail-shop-info"),
-        $window    = $(window),
-        offset     = $sidebar.offset(),
-        topPadding = 15;
-
-    $window.scroll(function() {
-        if ($window.scrollTop() > offset.top) {
-            $sidebar.stop().animate({
-                marginTop: $window.scrollTop() - offset.top + topPadding
-            });
-        } else {
-            $sidebar.stop().animate({
-                marginTop: 0
-            });
-        }
+    $(function() {
+        var $sidebar   = $(".detail-shop-info"),
+            $window    = $(window),
+            offset     = $sidebar.offset(),
+            topPadding = 88,
+            sidebarH = $(".detail-shop-inner").height(),
+            detailH = $(".detail-shop").height() - sidebarH;
+            //console.log(sidebarH);
+            //console.log(detailH);
+        $window.scroll(function() {
+            if ($window.scrollTop() > offset.top) {
+                $sidebar.stop().animate({
+                    marginTop: $window.scrollTop() - offset.top + topPadding
+                });
+            } else {
+                $sidebar.stop().animate({
+                    marginTop: 0
+                });
+            }
+            if ($window.scrollTop() > detailH ) {
+                $sidebar.stop().animate({
+                    marginTop: detailH
+                });
+            }
+        });
     });
-
-});
-
-    // var shopInfo = $('.detail-shop-info');
-    // var shopInfoTop = 88;
-    // shopInfo.css('top', $(window).height() );
-    //     $(document).ready(function(){
-    //     shopInfo.animate( { "top": $(document).scrollTop() + shopInfoTop +"px" }, 500 );
-    //     $(window).scroll(function(){
-    //     shopInfo.stop();
-    //     shopInfo.animate( { "top": $(document).scrollTop() + shopInfoTop + "px" }, 1000 );
-    //     });
-    // });
 
     // Review
     // --------------------------------------------------
