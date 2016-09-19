1bc65570d8c6e65739c6c718e2f606b14e97b1c1
jyeon <jyeon@isecommerce.co.kr>
2016-09-19 17:53:18 +0900

    모바일 셋팅

---
+++ mobile/footer/footer.php
@@ -0,0 +1,46 @@
+<footer>
+    <div class="container">
+        <ul class="footer-menu">
+            <li>
+                <h3 class="footer-title">general</h3>
+                <ul class="footer-link">
+                    <li><a href="#">store locator</a></li>
+                    <li><a href="#">showroom</a></li>
+                    <li><a href="#">careers</a></li>
+                    <li><a href="#">trade in</a></li>
+                </ul>
+            </li>
+            <li>
+                <h3 class="footer-title">customer care</h3>
+                <ul class="footer-link">
+                    <li><a href="#">odering & shipping</a></li>
+                    <li><a href="#">return policy</a></li>
+                    <li><a href="#">order status</a></li>
+                </ul>
+            </li>
+            <li>
+                <h3 class="footer-title">accounts</h3>
+                <ul class="footer-link">
+                    <li><a href="#">create an account</a></li>
+                    <li><a href="#">log in</a></li>
+                </ul>
+            </li>
+        </ul>
+        <div class="footer-sharing-link">
+            <h3 class="footer-title">follow us on</h3>
+            <ul>
+                <li><a href="https://www.facebook.com/stevenalancollection" target="_blank"><i class="icon-facebook"></i></a></li>
+                <li><a href="https://www.instagram.com/stevenalan/" target="_blank"><i class="icon-instagram"></i></a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="copyright">
+        <div class="container">
+            <p>COPYRIGHT © 2016 ISE COMMERCE. ALL RIGHTS RESERVED.</p>
+            <a href="#">Privacy Policy <i class="icon-right-open-big"></i></a>
+            <a href="#">Terms of Use <i class="icon-right-open-big"></i></a>
+        </div>
+    </div>
+</footer>
+</body>
+</html>
