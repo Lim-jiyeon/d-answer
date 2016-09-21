863bb5da44e13eda2573d1788fcd041627b26d43
jyeon <jyeon@isecommerce.co.kr>
2016-09-21 18:16:52 +0900

    메인메뉴 작업중

--- mobile/header/header.php
+++ mobile/header/header.php
@@ -10,94 +10,65 @@
     <script type="text/javascript" src="../include/js/custom.js"></script>
     <link rel="stylesheet" href="../include/css/styles.css">
 </head>
-<body>
+<body class="menu-open">
 <header>
-    <i class="icon-sliders"></i>
-    <i class="icon-cancel"></i>
-    <div class="site-utility">
-        <ul>
-            <li><a href="#">login</a></li>
-            <li class="menu-mypage">
-                <a href="#" class="link-mypage">mypage<i class="icon-down-dir"></i></a>
-                <ul class="mypage-dropdown">
-                    <li class="mypage-item"><a href="#">주문/배송 조회</a></li>
-                    <li class="mypage-item"><a href="#">위시리스트</a></li>
-                    <li class="mypage-item"><a href="#">1:1문의</a></li>
-                </ul>
-            </li>
-        </ul>
-    </div>
-    <div class="quick-access">
+    <div class="top">
         <h1 class="site-brand">
             <a href="#" title="stevenalan" class="center-block">
-                <img src="../images/common/brand-logo.png" alt="steven alan">
+                <img src="../include/images/brand-logo.png" alt="steven alan">
             </a>
         </h1>
         <div class="mobile-menu">
             <a href="#"><i class="icon-menu"></i></a>
         </div>
-        <div class="search">
-            <a href="#" onclick="layerOpen('.form-search');"><i class="icon-search"></i></a>
-        </div>
-        <div class="shoppingBag">
-            <a href="#">
-                <i class="icon-shoppingbag"></i>
-                <span class="total-badge">3</span>
-            </a>
-        </div>
     </div>
     <!-- Sidebar Menu -->
     <!-- [D] 2뎁스 메뉴가 있을 경우 child-item 클래스 추가 -->
     <div class="mobile-menu-wrap">
-        <ul class="sidebar-menu level-1">
-            <li class="menu-item"><a href="#">collection</a></li>
-            <li class="menu-item"><a href="#">new arrivals</a></li>
-            <li class="menu-item child-item">
-                <a href="#">women's</a>
-                <ul class="level-2 child-item">
-                    <li><a href="#">designers a-z</a></li>
-                    <li><a href="#">clothing</a></li>
-                    <li><a href="#">bags</a></li>
-                    <li class="child-item">
-                        <a href="#">accessories</a>
-                        <ul class=" level-3">
-                            <li><a href="#">sunglasses</a></li>
-                            <li><a href="#">socks &amp; tights</a></li>
-                            <li><a href="#">hair accessories</a></li>
-                            <li><a href="#">hats, gloves &amp; scarves</a></li>
-                            <li><a href="#">belts</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </li>
-            <li class="menu-item"><a href="#">men's</a></li>
-            <li class="menu-item"><a href="#">watches</a></li>
-            <li class="menu-item child-item">
-                <a href="#">jewelry</a>
-                <ul class="level-2 child-item">
-                    <li><a href="#">designers a-z</a></li>
-                    <li><a href="#">clothing</a></li>
-                    <li><a href="#">bags</a></li>
-                    <li><a href="#">accessories</a></li>
-                </ul>
-            </li>
-            <li class="menu-item"><a href="#">kid's</a></li>
-            <li class="menu-item"><a href="#">home</a></li>
-        </ul>
+        <div class="sidebar-menu">
+            <ul class="quick-access">
+               <li><a href="#">cart</a></li>
+               <li><a href="#">wish list</a></li>
+               <li><a href="#">my page</a></li>
+            </ul>
+            <ul class="level-1">
+                <li class="menu-item"><a href="#">collection</a></li>
+                <li class="menu-item"><a href="#">new arrivals</a></li>
+                <li class="menu-item child-item">
+                    <a href="#">women's</a>
+                    <ul class="level-2 child-item">
+                        <li><a href="#">designers a-z</a></li>
+                        <li><a href="#">clothing</a></li>
+                        <li><a href="#">bags</a></li>
+                        <li class="child-item">
+                            <a href="#">accessories</a>
+                            <ul class=" level-3">
+                                <li><a href="#">sunglasses</a></li>
+                                <li><a href="#">socks &amp; tights</a></li>
+                                <li><a href="#">hair accessories</a></li>
+                                <li><a href="#">hats, gloves &amp; scarves</a></li>
+                                <li><a href="#">belts</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li class="menu-item"><a href="#">men's</a></li>
+                <li class="menu-item"><a href="#">watches</a></li>
+                <li class="menu-item child-item">
+                    <a href="#">jewelry</a>
+                    <ul class="level-2 child-item">
+                        <li><a href="#">designers a-z</a></li>
+                        <li><a href="#">clothing</a></li>
+                        <li><a href="#">bags</a></li>
+                        <li><a href="#">accessories</a></li>
+                    </ul>
+                </li>
+                <li class="menu-item"><a href="#">kid's</a></li>
+                <li class="menu-item"><a href="#">home</a></li>
+            </ul>
+        </div>
         <a href="#" class="menu-close"><i class="icon-left"></i></a>
         <div class="menu-overlay"></div>
     </div>
     <!-- Sidebar Menu //end -->
-    <!-- Search Form Layer -->
-    <div class="form-search">
-        <div class="placeholder">
-            <label for="search_word" class="search-word form-label">SEARCH<i class="icon-search"></i></label>
-            <input type="text" id="search_word" class="form-control">
-        </div>
-        <a href="#" class="form-search-close" onclick="layerClose(this);"><i class="icon-cancel-thin"></i></a>
-    </div>
-    <!-- Search Form Layer //end -->
-    <div class="link-sale">
-        <a href="#">GRAND OPEN! 30%</a>
-    </div>
 </header>
