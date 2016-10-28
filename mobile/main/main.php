b4f3b11963b4f6f8d369845bd7a1a29323b278a0
jyeon <jyeon@isecommerce.co.kr>
2016-10-29 01:10:25 +0900

    20161028 수정사항 반영 중

--- mobile/main/main.php
+++ mobile/main/main.php
@@ -37,126 +37,129 @@
         <!-- [D] view type(Grid) : grid-view
                  view type(list) : list-view
                  각각 view type 에 맞게 클래스 추가 -->
-        <ul class="product-list grid-view">
-            <li class="list-item">
-                <a href="#">
-                    <img src="../include/images/temp/temp-product.jpg" alt="상품이미지">
-                    <div class="product-info">
-                        <ul class="color-picker">
-                            <li style="background-color:#000;"></li>
-                            <li style="background-color:#626262;"></li>
-                            <li style="background-color:#cfa972;"></li>
-                        </ul>
-                        <h4 class="product-name">SLP 자라 스탠원피스</h4>
-                        <div class="product-price">
-                            <em class="discount">89,000</em>
-                            <span class="price">84,000</span>
+        <div class="product-list grid-view">
+            <ul>
+                <li class="list-item">
+                    <a href="#">
+                        <img src="../include/images/temp/temp-product.jpg" alt="상품이미지">
+                        <div class="product-info">
+                            <ul class="color-picker">
+                                <li style="background-color:#000;"></li>
+                                <li style="background-color:#626262;"></li>
+                                <li style="background-color:#cfa972;"></li>
+                            </ul>
+                            <h4 class="product-name">SLP 자라 스탠원피스</h4>
+                            <div class="product-price">
+                                <em class="discount">89,000</em>
+                                <span class="price">84,000</span>
+                            </div>
                         </div>
-                    </div>
-                </a>
-            </li>
-            <li class="list-item">
-                <a href="#">
-                    <img src="../include/images/temp/temp-product.jpg" alt="상품이미지">
-                    <div class="product-info soldout">
-                        <h4 class="product-name">품절</h4>
-                    </div>
-                </a>
-            </li>
-            <li class="list-item">
-                <a href="#">
-                    <img src="../include/images/temp/temp-product.jpg" alt="상품이미지">
-                    <div class="product-info">
-                        <ul class="color-picker">
-                            <li style="background-color:#000;"></li>
-                            <li style="background-color:#626262;"></li>
-                            <li style="background-color:#cfa972;"></li>
-                        </ul>
-                        <h4 class="product-name">SLP 자라 스탠원피스 SLP 자라 스탠원피스 SLP 자라 스탠원피스</h4>
-                        <div class="product-price">
-                            <em class="discount">89,000</em>
-                            <span class="price">84,000</span>
+                        <div class="sale-badge" style="background-image:url('http://d-answer.com/shopimages/wooing102/pro_discount_495.gif');"></div>
+                    </a>
+                </li>
+                <li class="list-item">
+                    <a href="#">
+                        <img src="../include/images/temp/temp-product.jpg" alt="상품이미지">
+                        <div class="product-info soldout">
+                            <h4 class="product-name">품절</h4>
                         </div>
-                    </div>
-                </a>
-            </li>
-            <li class="list-item">
-                <a href="#">
-                    <img src="../include/images/temp/temp-product.jpg" alt="상품이미지">
-                    <div class="product-info">
-                        <ul class="color-picker">
-                            <li style="background-color:#000;"></li>
-                            <li style="background-color:#626262;"></li>
-                            <li style="background-color:#cfa972;"></li>
-                        </ul>
-                        <h4 class="product-name">SLP 자라 스탠원피스</h4>
-                        <div class="product-price">
-                            <em class="discount">89,000</em>
-                            <span class="price">84,000</span>
+                    </a>
+                </li>
+                <li class="list-item">
+                    <a href="#">
+                        <img src="../include/images/temp/temp-product.jpg" alt="상품이미지">
+                        <div class="product-info">
+                            <ul class="color-picker">
+                                <li style="background-color:#000;"></li>
+                                <li style="background-color:#626262;"></li>
+                                <li style="background-color:#cfa972;"></li>
+                            </ul>
+                            <h4 class="product-name">SLP 자라 스탠원피스 SLP 자라 스탠원피스 SLP 자라 스탠원피스</h4>
+                            <div class="product-price">
+                                <em class="discount">89,000</em>
+                                <span class="price">84,000</span>
+                            </div>
                         </div>
-                    </div>
-                </a>
-            </li>
-            <li class="list-item">
-                <a href="#">
-                    <img src="../include/images/temp/temp-product.jpg" alt="상품이미지">
-                    <div class="product-info soldout">
-                        <h4 class="product-name">품절</h4>
-                    </div>
-                </a>
-            </li>
-            <li class="list-item">
-                <a href="#">
-                    <img src="../include/images/temp/temp-product.jpg" alt="상품이미지">
-                    <div class="product-info">
-                        <ul class="color-picker">
-                            <li style="background-color:#000;"></li>
-                            <li style="background-color:#626262;"></li>
-                            <li style="background-color:#cfa972;"></li>
-                        </ul>
-                        <h4 class="product-name">SLP 자라 스탠원피스</h4>
-                        <div class="product-price">
-                            <em class="discount">89,000</em>
-                            <span class="price">84,000</span>
+                    </a>
+                </li>
+                <li class="list-item">
+                    <a href="#">
+                        <img src="../include/images/temp/temp-product.jpg" alt="상품이미지">
+                        <div class="product-info">
+                            <ul class="color-picker">
+                                <li style="background-color:#000;"></li>
+                                <li style="background-color:#626262;"></li>
+                                <li style="background-color:#cfa972;"></li>
+                            </ul>
+                            <h4 class="product-name">SLP 자라 스탠원피스</h4>
+                            <div class="product-price">
+                                <em class="discount">89,000</em>
+                                <span class="price">84,000</span>
+                            </div>
                         </div>
-                    </div>
-                </a>
-            </li>
-            <li class="list-item">
-                <a href="#">
-                    <img src="../include/images/temp/temp-product.jpg" alt="상품이미지">
-                    <div class="product-info">
-                        <ul class="color-picker">
-                            <li style="background-color:#000;"></li>
-                            <li style="background-color:#626262;"></li>
-                            <li style="background-color:#cfa972;"></li>
-                        </ul>
-                        <h4 class="product-name">SLP 자라 스탠원피스</h4>
-                        <div class="product-price">
-                            <em class="discount">89,000</em>
-                            <span class="price">84,000</span>
+                    </a>
+                </li>
+                <li class="list-item">
+                    <a href="#">
+                        <img src="../include/images/temp/temp-product.jpg" alt="상품이미지">
+                        <div class="product-info soldout">
+                            <h4 class="product-name">품절</h4>
                         </div>
-                    </div>
-                </a>
-            </li>
-            <li class="list-item">
-                <a href="#">
-                    <img src="../include/images/temp/temp-product.jpg" alt="상품이미지">
-                    <div class="product-info">
-                        <ul class="color-picker">
-                            <li style="background-color:#000;"></li>
-                            <li style="background-color:#626262;"></li>
-                            <li style="background-color:#cfa972;"></li>
-                        </ul>
-                        <h4 class="product-name">SLP 자라 스탠원피스</h4>
-                        <div class="product-price">
-                            <em class="discount">89,000</em>
-                            <span class="price">84,000</span>
+                    </a>
+                </li>
+                <li class="list-item">
+                    <a href="#">
+                        <img src="../include/images/temp/temp-product.jpg" alt="상품이미지">
+                        <div class="product-info">
+                            <ul class="color-picker">
+                                <li style="background-color:#000;"></li>
+                                <li style="background-color:#626262;"></li>
+                                <li style="background-color:#cfa972;"></li>
+                            </ul>
+                            <h4 class="product-name">SLP 자라 스탠원피스</h4>
+                            <div class="product-price">
+                                <em class="discount">89,000</em>
+                                <span class="price">84,000</span>
+                            </div>
                         </div>
-                    </div>
-                </a>
-            </li>
-        </ul>
+                    </a>
+                </li>
+                <li class="list-item">
+                    <a href="#">
+                        <img src="../include/images/temp/temp-product.jpg" alt="상품이미지">
+                        <div class="product-info">
+                            <ul class="color-picker">
+                                <li style="background-color:#000;"></li>
+                                <li style="background-color:#626262;"></li>
+                                <li style="background-color:#cfa972;"></li>
+                            </ul>
+                            <h4 class="product-name">SLP 자라 스탠원피스</h4>
+                            <div class="product-price">
+                                <em class="discount">89,000</em>
+                                <span class="price">84,000</span>
+                            </div>
+                        </div>
+                    </a>
+                </li>
+                <li class="list-item">
+                    <a href="#">
+                        <img src="../include/images/temp/temp-product.jpg" alt="상품이미지">
+                        <div class="product-info">
+                            <ul class="color-picker">
+                                <li style="background-color:#000;"></li>
+                                <li style="background-color:#626262;"></li>
+                                <li style="background-color:#cfa972;"></li>
+                            </ul>
+                            <h4 class="product-name">SLP 자라 스탠원피스</h4>
+                            <div class="product-price">
+                                <em class="discount">89,000</em>
+                                <span class="price">84,000</span>
+                            </div>
+                        </div>
+                    </a>
+                </li>
+            </ul>
+        </div>
         <a href="#" class="list-more"></a>
         <!-- Category List //end -->
     </div>
