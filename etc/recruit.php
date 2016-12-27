875c1ef9156ab922e1ef3b753e12fbc6d38d159d
jyeon <jyeon@isecommerce.co.kr>
2016-12-27 17:33:33 +0900

    20161227 - PC버전 회사소개 화면

---
+++ etc/recruit.php
@@ -0,0 +1,20 @@
+<?php
+    include ("../header/header.php");
+?>
+
+    <div class="container">
+        <div class="page-container">
+            <ul class="menu-company">
+                <li><a href="./introduction.php">회사소개</a></li>
+                <li><a href="./recruit.php">인재채용</a></li>
+                <li><a href="./partnership.php">제휴제안</a></li>
+            </ul>
+            <div class="content-company">
+                <img src="../include/images/recruit.jpg">
+            </div>
+        </div>
+    </div>
+
+<?php
+    include ("../footer/footer.php");
+?>
